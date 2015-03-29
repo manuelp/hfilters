1a4a6fcfe47aebbe4118a4d33320e6232f0e34a1
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2015-03-29 23:35:34 +0200

    Simplified code.

--- Filters.hs
+++ Filters.hs
@@ -12,7 +12,7 @@ data Filter = IntF String Int
 -- always returns False. This way when we write our instances we only
 -- need to specify the real tests.
 class Filterable d where
-    matches :: d -> Filter -> Bool
+    matches :: Filter -> d -> Bool
     matches _ _ = False
 
 -- As an example, we define a record type to use for our experiments.
@@ -26,19 +26,19 @@ data Person = Person {
 -- We make Person values Filterable by defining the available filters
 -- and how they are computed.
 instance Filterable Person where
-    matches p (IntF "age" v) = (pAge p) == v
-    matches p (StrF "name" v) = (pName p) == v
-    matches p (StrF "surname" v) = (pSurname p) == v
-    matches p (BoolF "is-a-superhero" v) = (isASuperHero p) == v
+    matches (IntF "age" v) p = (pAge p) == v
+    matches (StrF "name" v) p = (pName p) == v
+    matches (StrF "surname" v) p = (pSurname p) == v
+    matches (BoolF "is-a-superhero" v) p = (isASuperHero p) == v
     matches _ _ = False
 
 -- Filtering data by using a filter can be defined as a generic
 -- function.
-filterData :: Filterable d => [d] -> Filter -> [d]
-filterData xs f = filter ((flip matches) f) xs
+filterData :: Filterable d => Filter -> [d] -> [d]
+filterData f = filter (matches f) 
 
 -- This way, filtering data using a list of filters can be defined
 -- generically as a fold.
 filterData' :: Filterable d => [d] -> [Filter] -> [d]
-filterData' xs fs = foldl filterData xs fs
+filterData' = foldr filterData
 
